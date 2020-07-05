51f1ca173ac33608da4729d3a9a50ca3b6daf09a
waleed.elbarbary <waleed.elbarbary@wirecard.com>
2020-07-05 15:39:13 +0200

    Add Cucumber options

--- src/main/java/Runner/testRunner.java
+++ src/main/java/Runner/testRunner.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "/Users/waleed.elbarbary/Downloads/BDDFrameWork/src/main/java/Features",//the path of the feature files
-        glue="stepDefinitions",//the path of the step definition files
-        format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"} //to generate different types of reporting
-
+        glue = "stepDefinitions",//the path of the step definition files
+        format = {"pretty", "html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"},//to generate different types of reporting
+        dryRun = false, //check mapping between step in feature file and step in step definition class
+        monochrome = true, // to generate pretty output
+        strict = true
 )
 
 
--- test-outout/report.js
+++ test-outout/report.js
@@ -48,42 +48,42 @@ formatter.match({
   "location": "loginStepDefinition.user_navigated_to_login_page()"
 });
 formatter.result({
-  "duration": 3846393030,
+  "duration": 3732860468,
   "status": "passed"
 });
 formatter.match({
   "location": "loginStepDefinition.title_of_login_page_is_Free_CRM()"
 });
 formatter.result({
-  "duration": 18887005,
+  "duration": 11076335,
   "status": "passed"
 });
 formatter.match({
   "location": "loginStepDefinition.user_Enter_UserName_Password()"
 });
 formatter.result({
-  "duration": 2999220235,
+  "duration": 1734081326,
   "status": "passed"
 });
 formatter.match({
   "location": "loginStepDefinition.user_click_login_button()"
 });
 formatter.result({
-  "duration": 78361963,
+  "duration": 26654796,
   "status": "passed"
 });
 formatter.match({
   "location": "loginStepDefinition.user_is_on_Home_page()"
 });
 formatter.result({
-  "duration": 36866270,
+  "duration": 5710741,
   "status": "passed"
 });
 formatter.match({
   "location": "loginStepDefinition.sign_out_and_close_the_browser()"
 });
 formatter.result({
-  "duration": 152382359,
+  "duration": 123721009,
   "status": "passed"
 });
 });
